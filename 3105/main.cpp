c9968d23d004b684519410b4c60b717dd6264992
pupiraresdi <34660424+pupiraresdi@users.noreply.github.com>
2021-11-29 16:57:43 +0200

    Cool

--- 3105/main.cpp
+++ 3105/main.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <bits/stdc++.h>
+
+std::ifstream in("multiplu.in");
+std::ofstream out("multiplu.out");
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
 
 int main() {
-    cout << "2";
-    return 0;
+    int a, b;
+    in >> a >> b;
+    out << gcd(a, b) << "\n";
 }
---
+++ 573/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+constexpr int N = 1e2;
+vector  <int> G[N+5], x;
+int de[N+5], di[N+5];
+
+int main() {
+    int n, m, u, v;
+    cin >> n >> m;
+    for( int i = 1; i <= m; ++i)
+        cin >> u >> v,
+        G[u].push_back(v);
+    for( int i = 1; i <= n; ++i) {
+        for( int j = 0; j < G[i].size(); ++j)
+            di[G[i][j]]++;
+        de[i] = G[i].size();
+    }
+    int nod = 0;
+    for( int i = 1; i <= n; ++i)
+    {
+        if(di[i] == de[i])
+            nod++,
+            x.push_back(i);
+    }
+    cout << nod << "\n";
+    for( int i = 0; i< nod; ++i)
+        cout << x[i] << " ";
+    return 0;
+ }
